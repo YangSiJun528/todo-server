4c7c2ca1debeef1d7757bca0a79b9efa4baf0ee3
YangSiJun528 <80192911+YangSiJun528@users.noreply.github.com>
2021-11-04 20:58:53 +0900

    add Repository 구현

---
+++ src/main/java/org/example/model/TodoEntity.java
@@ -0,0 +1,26 @@
+package org.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "todoOrder", nullable = false)
+    private Long order;
+
+    @Column(nullable = false)
+    private Boolean completed;
+}
